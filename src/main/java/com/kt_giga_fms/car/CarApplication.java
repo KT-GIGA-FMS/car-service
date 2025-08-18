e8617ec8adc48cc946eb03a814ac3a4f7a0fb1e7
rlfrkdms1 <rlfrkdms1@naver.com>
2025-08-18 17:18:08 +0900

    [FEAT]: Spring 프로젝트 세팅

---
+++ src/main/java/com/kt_giga_fms/car/CarApplication.java
@@ -0,0 +1,13 @@
+package com.kt_giga_fms.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarApplication.class, args);
+	}
+
+}
